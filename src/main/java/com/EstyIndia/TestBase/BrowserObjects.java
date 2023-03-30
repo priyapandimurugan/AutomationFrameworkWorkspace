fecca8dd5f176a1b06f86078bd669ae37e02c015
Vishwanathan <vishwanathan.mathesan07@gmail.com>
2023-03-30 11:57:51 +0530

    Added initial framework

---
+++ src/main/java/com/EstyIndia/TestBase/BrowserObjects.java
@@ -0,0 +1,9 @@
+package com.EstyIndia.TestBase;
+
+public class BrowserObjects {
+	public static final String URL = "https://www.qaetsy.com/";
+	public static final int MAX_EXWAIT = 10;
+	public static final int MAX_WAIT = 30;
+	public static final String BROWSER = "Chrome";
+
+}
---
+++ src/main/java/com/EstyIndia/TestBase/EstyIndiatestbase.java
@@ -0,0 +1,54 @@
+package com.EstyIndia.TestBase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import com.EstyIndia.HelperAction.ExtentReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EstyIndiatestbase extends ExtentReporter {
+
+	@BeforeClass
+	public static WebDriver lanuchbrowser() {
+		switch (BrowserObjects.BROWSER) {
+		case "Chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(90));
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(BrowserObjects.MAX_WAIT));
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(BrowserObjects.MAX_WAIT));
+			driver.get(BrowserObjects.URL);
+			break;
+
+		case "Firefox":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(90));
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(BrowserObjects.MAX_WAIT));
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(BrowserObjects.MAX_WAIT));
+			driver.get(BrowserObjects.URL);
+			break;
+
+		case "Edge":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(90));
+			driver.manage().window().maximize();
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(BrowserObjects.MAX_WAIT));
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(BrowserObjects.MAX_WAIT));
+			driver.get(BrowserObjects.URL);
+			break;
+		}
+		return driver;
+
+	}
+
+}
